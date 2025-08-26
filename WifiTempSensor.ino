769180cd76cc986414f52c292c109930d8735637
udfowner <javavirys@gmail.com>
2025-08-26 15:12:19 +0300

    Update WifiTempSensor.ino

--- WifiTempSensor.ino
+++ WifiTempSensor.ino
@@ -1,3 +1,21 @@
+/*
+ *    Copyright 2025 UDFOwner
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    More details: https://udfsoft.com/
+ */
+
 #include "DHT.h"
 
 #include <WiFi.h>
@@ -6,8 +24,8 @@
 #include <WiFiClientSecure.h>
 #include <HTTPClient.h>
 
-#define API_KEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  // YOUR API Key
-#define DEVICE_ID "xxxx-xxxx-xxxx-xxxx-xxxx"  // YOUR DEVICE ID
+#define API_KEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  // YOUR API Key, to get it write to us: support@udfsoft.com
+#define DEVICE_ID "xxxx-xxxx-xxxx-xxxx-xxxx"  // YOUR DEVICE ID, to get it write to us: support@udfsoft.com
 
 
 #define LED_PIN 8
